27777020206c991d841a47bcb57365706e20119a
Amal Antony <amalantony06@gmail.com>
2015-10-30 22:03:45 +0530

    Show only relevant chirps on the timeline

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ var ChirpStore = require('../stores/chirps.js');
 var Home = React.createClass({
 	getInitialState: function() {
 		return {
-			chirps: ChirpStore.all()
+			chirps: ChirpStore.timeline()
 		};
 	},
 	componentDidMount: function() {
--- src/stores/chirps.js
+++ src/stores/chirps.js
@@ -1,10 +1,16 @@
 "use strict";
 var constants = require('../constants');
-
+var UserStore = require('./users');
 var ChirpStore = require('./store').extend({
 	init: function() {
 		this.bind(constants.GOT_CHIRPS, this.set);
 		this.bind(constants.CHIRPED, this.add);
+	},
+	timeline: function() {
+		var ids = [UserStore.currentUser.cid].concat(UserStore.currentUser.following);
+		return this._data.filter(function(chirp) {
+			return ids.indexOf(chirp.userId) > -1;
+		});
 	}
 });
 
